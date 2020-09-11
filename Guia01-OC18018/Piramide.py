b7f452e01cfc942bd9ffc8749839abb920776452
OC18018 <OC18018@ues.edu.sv>
2020-09-10 19:22:28 -0600

    Guia01-OC18018 commit 2

--- Guia01-OC18018/Piramide.py
+++ Guia01-OC18018/Piramide.py
@@ -11,14 +11,12 @@ try:
         for b in range(cuadros):
             #Validacion de piramide normal
             if (b * (b + 1) / 2) == cuadros:
-                print('la base es ', b)
                 for i in range(1, b + 1):
                     print(" " * (b - i), end="")
                     print(" *" * i)
                 bandera = False
             #Valida si es una piramide donde sobran cuadros
             elif (b * (b + 1) / 2) > cuadros and bandera == True:
-                print('la base es ', b)
                 contador =0
                 for i in range(1, b):
                     print(" " * (b - i), end="")
--- Guia01-OC18018/binario.py
+++ Guia01-OC18018/binario.py
@@ -1,3 +1,4 @@
+#OC18018-Osorio Clemente,José Armando
 try:
     print(format(int(input("Ingrese numero Decimal:")), "0b"))
 except ValueError as e:
