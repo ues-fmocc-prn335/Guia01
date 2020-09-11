d8db73c28ec235e5dc9d29413ee7b8e17f2f32f3
k1000pro <70303778+k1000pro@users.noreply.github.com>
2020-09-10 19:44:40 -0600

    Update Guia01-MM19131.py

--- Guia01-MM19131.py
+++ Guia01-MM19131.py
@@ -1,18 +1,39 @@
+import math
+
 #Centrar texto
-frase=input("Escriba una frase o palabra\n").upper()
+print("CENTRAR TEXTO")
+frase=input("Escriba una frase o palabra:\n").upper()
 print(frase.center(160," "))
 
 
 #Encontrar la mediana
-num1 = input("Escribe el primer nùmero:\n")
-num2 = input("Escribe el segundo nùmero:\n")
-num3 = input("Escribe el tercero nùmero:\n")
-lista = [int(num1), int(num2), int(num3)]
+print("ENCONTRAR LA MEDIANA DE TRES NUMEROS")
+num=0
+lista = []
+while(num<3):
+    try:
+        numero = int(input("Escribe un nùmero:\n"))
+        if(math.isnan(numero) == False):
+            lista.append(numero)
+            num=num+1
+        else:
+            print("Debe escribir un numero")
+    except:
+        print("Debe escribir un numero")
 lista.sort()
-print(f"La mediana es:  {lista[1]}")
+print(f"La mediana es: {lista[1]}")
 
 
 #conversor de dedcimal a binario
-numero = input("Escribe el numero que quieres convertir:\n")
-
-print("{0:#b}".format(int(numero)).replace("b",""))
+print("CONVERSOR DE DECIMAL A BINARIO")
+validar = False
+while(validar==False):
+    try:
+        numero = int(input("Escribe el numero que quieres convertir:\n"))
+        if(math.isnan(numero)==False):
+            validar = True
+            print("{0:#b}".format(int(numero)).replace("b",""))
+        else:
+            print("Debe escribir un numero")
+    except:
+        print("Debe escribir un numero")
