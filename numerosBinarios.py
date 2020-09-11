34bd0a9fb1b5db3d64346a063096b456991a644d
elmerint <ie18001@ues.edu.sv>
2020-09-11 00:45:32 -0600

    Guia 01 IE18001

---
+++ numerosBinarios.py
@@ -0,0 +1,5 @@
+#convertir un numero decimal a binario
+print("Convertir un número decimal a binario")
+numero= int(input("Ingrese el número que desea convertir: "))
+print("El resultado es:")
+print(bin(numero))
