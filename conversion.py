1c5c8417b7207497ad2c77d5ece29ec3d776752e
jm1299 <josemdg12@gmail.com>
2020-09-10 22:33:15 -0300

    Creo la funcion para convertir de decimal a binario

---
+++ conversion.py
@@ -0,0 +1,11 @@
+def Convertir(decimal):
+# inicializo la variable
+    binario = ''
+
+# calculo
+    while decimal // 2 != 0:
+        binario = str(decimal % 2) + binario
+        decimal = decimal // 2
+
+# devuelvo el valor
+    return str(decimal) + binario
